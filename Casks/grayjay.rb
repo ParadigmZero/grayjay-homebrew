2363b6f81b2090394eb92c9bb15b58a200cb19c8
ParadigmZero <josephrcalton@gmail.com>
2025-03-21 13:35:31 +0000

    correct the url

--- Casks/grayjay.rb
+++ Casks/grayjay.rb
@@ -1,7 +1,7 @@
 cask "grayjay" do
   version "5"
   sha256 "f1081576dcea0cd4f057ebec824f73681a787b9b6a0f13e61ce1b3e58d265a31"
-  url "https://github.com/futo-org/Grayjay.Desktop/archive/refs/tags/#{version}.zip"
+  url "https://updater.grayjay.app/Apps/Grayjay.Desktop/Grayjay.Desktop-osx-arm64.zip"
   name "Grayjay desktop alpha"
   desc "A modern video player for various platforms (alpha)"
   homepage "https://grayjay.app/desktop/"
