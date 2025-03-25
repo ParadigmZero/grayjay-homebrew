9430a861811db9e1cf258d1a351b864ccd5f563f
ParadigmZero <josephrcalton@gmail.com>
2025-03-25 12:05:10 +0000

    big fix, comma missing, stopping rb script

--- Casks/grayjay.rb
+++ Casks/grayjay.rb
@@ -1,7 +1,7 @@
 cask "grayjay" do
   arch arm: "arm64", intel: "x64"
   version "5"
-  sha256 arm: "f1081576dcea0cd4f057ebec824f73681a787b9b6a0f13e61ce1b3e58d265a31"
+  sha256 arm: "f1081576dcea0cd4f057ebec824f73681a787b9b6a0f13e61ce1b3e58d265a31",
   intel: 
   "530c469bcd59b446857404cbb332ac2e96cda0d3b90b8d5d085236dbb8a78eff"
   url "https://updater.grayjay.app/Apps/Grayjay.Desktop/Grayjay.Desktop-osx-#{arch}.zip"
